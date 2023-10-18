7f46b7de1c003153aa61b6bb171ee40727889976
Andy Jocham <andy@jocham.xyz>
2023-10-18 09:15:37 +0200

    umlaute gefixt

--- src/coffeeshop/CoffeeConstants.java
+++ src/coffeeshop/CoffeeConstants.java
@@ -6,9 +6,9 @@ public class CoffeeConstants {
     public static final String LAST = "Last";
     public static final String PREVIOUS = "Previous";
     public static final String FIRST = "First";
-    public static final String INSERT = "Einfügen";
+    public static final String INSERT = "EinfÃ¼gen";
     public static final String ADD = "Add";
-    public static final String DELETE = "Löschen";
+    public static final String DELETE = "LÃ¶schen";
     public static final String SELECT = "Anzeigen";
     public static final String REFRESH = "refresh";
     public static final String UPDATE = "Aktualisieren";
